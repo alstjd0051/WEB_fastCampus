34579c0857578d884e6a3961695af3d460b9a611
miiin_sseong <70365399+alstjd0051@users.noreply.github.com>
2021-06-05 03:53:25 +0900

    타이머 함수

--- JS_TEST/main.js
+++ JS_TEST/main.js
@@ -53,7 +53,7 @@ console.log('doubleArrow', doubleArrow(7)) */
 
 // 즉시실행함수
 // IIFE, Immediately-Invoked Function Expression
-const a = 7
+/* const a = 7
 
 function double() {
   console.log(a * 2)
@@ -62,4 +62,49 @@ double();
 
 (function () {
   console.log(a * 2)
-})()
+})();
+
+(function () {
+  console.log(a * 2)
+})();
+
+(function () {
+  console.log(a * 2)
+}()); */
+
+//호이스팅(Hoisting)
+// 함수 선언부가 유효범위 최상단으로 끌어올려지는 현상
+
+/* const a =7
+
+const double = function () {
+  console.log(a*2)
+}
+double() */
+
+// 타이머 함수
+// setTimeout(함수, 시간) : 일정 시간 후 함수 실행
+// setInterval(함수, 시간) : 시간 간격마다 함수 실행
+//  clearTimeOut() : 설정된 Timeout 함수를 종료
+//  clearInterval() : 설정된 Interval 함수를 종료
+
+/* setTimeout( () => {
+  console.log('miiin_sseong')
+}, 3000) */
+
+/* const timer = setTimeout(() => {
+  console.log('miiin_sseong')
+}, 3000) */
+
+const timer = setInterval(() => {
+  console.log('miiin_sseong')
+}, 3000)
+
+/* const h1El = document.querySelector('h1')
+h1El.addEventListener('click', () => {
+  clearTimeout(timer)
+}) */
+const h1El = document.querySelector('h1')
+h1El.addEventListener('click', () => {
+  clearInterval(timer)
+})
